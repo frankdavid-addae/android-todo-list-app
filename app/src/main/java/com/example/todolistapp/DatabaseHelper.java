d8384dc01920de1f6f1e5b902232cd01b5ac28ff
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-12 13:17:56 +0000

    added database helper class

---
+++ app/src/main/java/com/example/todolistapp/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.todolistapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "TodosDatabase";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sqlQuery = "CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)";
+        sqLiteDatabase.execSQL(sqlQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sqlQuery = "DROP TABLE IF EXISTS Todo";
+        sqLiteDatabase.execSQL(sqlQuery);
+        onCreate(sqLiteDatabase);
+    }
+}
