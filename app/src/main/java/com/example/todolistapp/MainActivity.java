d70d119185286d51f69dc77cb49b0d653af7fe46
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-12 17:27:39 +0000

    swipe to delete functionality

--- app/src/main/java/com/example/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.todolistapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -66,6 +68,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         rvTodos.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        ItemTouchHelper.SimpleCallback itemTouchCallback = new ItemTouchHelper.SimpleCallback(
+                0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(
+                    @NonNull RecyclerView recyclerView,
+                    @NonNull RecyclerView.ViewHolder viewHolder,
+                    @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                new TodoHandler(MainActivity.this).deleteTodo(todos.get(viewHolder.getAdapterPosition()).getId());
+                todos.remove(viewHolder.getAdapterPosition());
+                todoAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(itemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(rvTodos);
 
         getTodos();
     }
