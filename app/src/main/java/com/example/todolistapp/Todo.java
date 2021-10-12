9f8ea8bdb0e530a30b7f2612c10b175a0c8c4374
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-12 13:05:11 +0000

    todo holder and todo adapter

--- app/src/main/java/com/example/todolistapp/Todo.java
+++ app/src/main/java/com/example/todolistapp/Todo.java
@@ -1,2 +1,36 @@
-package com.example.todolistapp;public class Todo {
+package com.example.todolistapp;
+
+public class Todo {
+    private int id;
+    private String title;
+    private String description;
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- app/src/main/java/com/example/todolistapp/TodoAdapter.java
+++ app/src/main/java/com/example/todolistapp/TodoAdapter.java
@@ -1,2 +1,54 @@
-package com.example.todolistapp;public class TodoAdapter {
+package com.example.todolistapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoHolder> {
+    ArrayList<Todo> todos;
+    Context context;
+
+    public TodoAdapter(ArrayList<Todo> todos, Context context) {
+        this.todos = todos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public TodoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.todo_holder, parent, false);
+        return new TodoHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodoHolder holder, int position) {
+        holder.tvTodoTitle.setText(todos.get(position).getTitle());
+        holder.tvTodoDescription.setText(todos.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return todos.size();
+    }
+
+    class TodoHolder extends RecyclerView.ViewHolder {
+        TextView tvTodoTitle, tvTodoDescription;
+        ImageView ivEdit;
+
+        public TodoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTodoTitle = itemView.findViewById(R.id.tvTodoTitle);
+            tvTodoDescription = itemView.findViewById(R.id.tvTodoDescription);
+            ivEdit = itemView.findViewById(R.id.ivEdit);
+        }
+    }
 }
